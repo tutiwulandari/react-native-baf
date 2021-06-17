c337129c23bfa3b9356117dcdd0a414b5cacac92
Tuti Wulandari <tutiwulandari150@gmail.com>
2021-06-17 23:04:35 +0700

    added styling

--- src/screen/TicketList.js
+++ src/screen/TicketList.js
@@ -1,25 +1,34 @@
-import React from 'react'
-import { View, Text} from 'react-native'
-import QRCode from 'react-native-qrcode-svg'
-import { event } from 'react-native-reanimated'
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
 
 const TicketList = ({route}) => {
+  const {event} = route.params;
 
-    const {event} = route.params;
-    
-    console.log("INI DATA", event)
-    return (
-        <View style={{backgroundColor:"powderblue", padding:70, margin:20}}>
-            <View style={{alignItems:'flex-end'}}>
-                <QRCode value={event.imageUrl} style={{alignItems:"flex-start"}}/>
-                <Text >{event.eventName}</Text>
-                <Text>{route.params.counter} </Text>
-                <Text>{route.params.counter * event.ticketPrice} </Text>
-
-            </View>
-            
+  console.log('INI DATA', event);
+  return (
+    <View style={{backgroundColor: 'powderblue', padding: 50, margin: 20}}>
+      <View style={{flexDirection: 'row'}}>
+        <View style={{marginRight:20}}>
+          <QRCode value={event.imageUrl} style={{alignItems: 'flex-start'}} />
         </View>
-    )
-}
+        <View>
+          <Text style={styles.textTitle}>Tiket: {event.eventName}</Text>
+          <Text style={styles.textTitle}>Jumlah Tiket: {route.params.counter} </Text>
+          <Text style={styles.textTitle}>Total Harga: {route.params.counter * event.ticketPrice}
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
 
-export default TicketList
+const styles = StyleSheet.create({
+  textTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom:15
+  },
+});
+
+export default TicketList;
